5698c32b46b375d479cbed88a810e214319f5ea8
Atreya Tiwari <atreyatiwari102@gmail.com>
2026-04-18 19:47:25 +0530

    NexTalk Frontend

--- nextalk-frontend/src/api/axios.js
+++ nextalk-frontend/src/api/axios.js
@@ -1,13 +1,18 @@
 import axios from "axios";
 
+const BASE_URL =
+  window.location.hostname === "localhost"
+    ? "http://localhost:8080"
+    : "https://nextalk-production-bd2f.up.railway.app";
+
 const api = axios.create({
-  baseURL: "http://localhost:8080",
+  baseURL: BASE_URL,
   headers: {
     "Content-Type": "application/json",
   },
 });
 
-// Auto attach token before every request
+// Auto attach token
 api.interceptors.request.use(
   (config) => {
     const token = localStorage.getItem("token");

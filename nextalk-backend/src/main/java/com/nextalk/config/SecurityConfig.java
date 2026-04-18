d27e9c9236c45896fa96ebe67e0e3fc8dd670bfe
Atreya Tiwari <atreyatiwari102@gmail.com>
2026-04-18 19:07:45 +0530

    NexTalk Backend

--- nextalk-backend/src/main/java/com/nextalk/config/SecurityConfig.java
+++ nextalk-backend/src/main/java/com/nextalk/config/SecurityConfig.java
@@ -62,7 +62,7 @@ public class SecurityConfig {
         CorsConfiguration configuration = new CorsConfiguration();
 
         configuration.setAllowedOrigins(
-                List.of("http://localhost:5173")
+                List.of("*")
         );
 
         configuration.setAllowedMethods(
